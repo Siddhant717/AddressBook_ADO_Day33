899e83ae0bd00aa229fd8f32ae1d69f852e482e6
Siddhant <santoshshrivastava39@gmail.com>
2022-07-24 01:26:43 +0530

    Insert Contacts to Address Book

--- AddressBookSystemADO/AddressBook.cs
+++ AddressBookSystemADO/AddressBook.cs
@@ -10,19 +10,22 @@ namespace AddressBookSystemADO
     public class AddressBook
     {
 
-        //Creating AddressBook Table
-        public void CreateAddressBookTable()
+        public void InsertContactInAddressBookTable()
         {
-            var SQL = @$"create table AddressBook ( FirstName Varchar(20), LastName Varchar(20), Address Varchar(50), City Varchar(20), State Varchar(20), ZipCode int, PhoneNumber bigint, Email Varchar(20) )";
+            var SQL = @$"insert into AddressBook Values  ('Siddhant','Shrivastava','A colony','Ujjain','MP',666666,9998889998,'Sid@gmail.com'),
+                                                         ('Riya','Sharma','Ab colony','Lucknow','UP',677666,9998882348,'Riya@gmail.com'),
+                                                         ('Pranav','Vyas','C colony','Pune','Maharashtra',656666,9923489998,'Pranav@gmail.com'),
+                                                         ('Anamika','Singh','D colony','Chennai','Tamil Naidu',662226,8898889998,'Anamika@gmail.com');";
             string connectingString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Address_Book_Service;Integrated Security=True";
             SqlConnection connection = new SqlConnection(connectingString);
             SqlCommand cmd = new SqlCommand(SQL, connection);
             connection.Open();
             int reader = cmd.ExecuteNonQuery();
             Console.WriteLine(reader);
-            Console.WriteLine("AddressBook Table Created Successfully");
+            Console.WriteLine("Command Completed Successfully");
             Console.ReadKey();
             connection.Close();
         }
+
     }
 }
