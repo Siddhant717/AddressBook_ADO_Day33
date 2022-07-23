6a75afe631a29649bedd211551ebb450ba5553b7
Siddhant <santoshshrivastava39@gmail.com>
2022-07-24 01:15:48 +0530

    Create a AddressBook Service Database

---
+++ AddressBookSystemADO/AddressBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystemADO
+{
+    public class AddressBook
+    {
+        public void CreateAddressBookServiceDB()
+        {
+            //creating AddressBookService Database
+            string SQL = "create database Address_Book_Service ";
+
+            string connectingstring = @"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True";
+
+            SqlConnection connection = new SqlConnection(connectingstring);
+            SqlCommand sqlcommand = new SqlCommand(SQL, connection);
+            try
+            {
+                connection.Open();
+                sqlcommand.ExecuteNonQuery();
+                Console.WriteLine("Database Created Successfully....");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception :" + e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+    }
+}
---
+++ AddressBookSystemADO/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBookSystemADO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressbook = new();
+            addressbook.CreateAddressBookServiceDB();
+
+        }
+    }
+}
